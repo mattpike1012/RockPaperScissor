4ab70748ef184b97ebc0f79738902acb616a2254
Matthew Pike s3865305 <s3865305@student.rmit.edu.au>
2021-12-09 15:41:24 +1100

    Finished playRound function

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 function computerPlay(){
-    const outcomes = ["Rock", "Paper", "Scissor"]
+    const outcomes = ["rock", "paper", "scissor"]
     return outcomes[Math.floor(Math.random() * 3)]
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    if ((playerSelection === "rock" && computerSelection === "rock") || (playerSelection === "paper" && computerSelection === "paper") || (playerSelection === "scissor" && computerSelection === "scissor")){
+        return "Tie!"
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper beats Rock"
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissor"){
+        return "You Win! Rock beats Scissor"
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "You Win! Paper beats Rock"
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissor"){
+        return "You Lose! Scissor beats Paper"
+    }
+    else if (playerSelection === "scissor" && computerSelection === "rock"){
+        return "You Lose! Rock beats Scissor"
+    }
+    else if (playerSelection === "scissor" && computerSelection === "paper"){
+        return "You Win! Scissor beats Paper"
+    }
 }
 
 // console.log(computerPlay())
